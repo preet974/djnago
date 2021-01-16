fce54d7fd18c0e42e70af9853ffa6f244eb718e2
Raman <ramandeep6785@gmail.com>
2021-01-16 17:11:04 +0530

    fist commit

---
+++ rec/quotes/our-quote/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+class App extends React.Component { 
+	render() { 
+		return( 
+			<div> 
+				<div> 
+					<div> 
+						<h1>Quote is going to be written here</h1> 
+						<footer>--- by 
+						<cite title="Source Title"> 
+							written by meeeee 
+						</cite> 
+						</footer> 
+					</div> 
+				</div> 
+			</div>); 
+	} 
+} 
+export default App;
